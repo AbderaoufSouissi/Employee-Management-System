9d6aa6ccddca4e1ef4059d527a93e119138f98cd
ars <abderaouf.souissi@outlook.com>
2025-03-04 15:39:39 +0100

    added employee dto

--- ems-backend/src/main/java/com/ars/ems_backend/EmsBackendApplication.java
+++ ems-backend/src/main/java/com/ars/ems_backend/EmsBackendApplication.java
@@ -1,13 +1,20 @@
 package com.ars.ems_backend;
 
+import com.ars.ems_backend.dto.EmployeeDto;
+import com.ars.ems_backend.entity.Employee;
+import com.ars.ems_backend.repository.EmployeeRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
+@Slf4j
 @SpringBootApplication
 public class EmsBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmsBackendApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(EmsBackendApplication.class, args);
 	}
+
 
 }
---
+++ ems-backend/src/main/java/com/ars/ems_backend/dto/EmployeeDto.java
@@ -0,0 +1,5 @@
+package com.ars.ems_backend.dto;
+
+
+public record EmployeeDto(Long id, String firstName, String lastName, String email) {
+}
